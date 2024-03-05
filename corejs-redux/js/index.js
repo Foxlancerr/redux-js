98e0c84de1b9515d8cdb158780d11e2179c7e805
M-Asimm <kpkprince55persia@gmail.com>
2024-03-05 10:55:43 +0500

    redux backend logics touched

--- corejs-redux/js/index.js
+++ corejs-redux/js/index.js
@@ -1,24 +1,78 @@
-const counter = 0;
+// const counter = 0;
 
-// console.log(counter + 1)
-// console.log(counter + 2)
-// console.log(counter + 3)
+// // console.log(counter + 1)
+// // console.log(counter + 2)
+// // console.log(counter + 3)
 
-const objCounter ={
-    count: 0
+// const objCounter ={
+//     count: 0
+// }
+
+// // mutable because point as refrence
+// // const previesState = objCounter
+// // console.log(previesState)
+
+// // objCounter.count = 4
+// // console.log(previesState)
+
+// //immutable
+// const previesState2 = {...objCounter} // shallow copy
+// console.log(previesState2)
+
+// objCounter.count = 10
+// console.log(previesState2)
+
+// let Student = {
+//   name: "kmaran",
+// };
+
+// let previesState = Student
+// console.log(Student);
+// function stateUpdate() {
+//   Student.name = Student.name + " khan";
+// }
+
+// stateUpdate();
+// console.log(previesState);
+// console.log(Student);
+
+// let counter = {
+//   count: 0,
+// };
+
+// console.log(counter);
+// counter.count = counter.count + 1;
+// console.log(counter);
+// counter.count = counter.count + 1;
+// console.log(counter);
+// counter.count = counter.count + 1;
+// console.log(counter);
+
+// console.log(counter);
+// function updateCounter() {
+//   counter.count = counter.count + 1;
+// }
+// updateCounter();
+// console.log(counter);
+// updateCounter();
+// console.log(counter);
+// updateCounter();
+// console.log(counter);
+
+let counter = {
+  count: 0,
+};
+let previesState  = counter
+
+// console.log(previesState);
+function updateCounter() {
+  return counter = { count: counter.count + 1 };
 }
-
-// mutable because point as refrence
-// const previesState = objCounter
-// console.log(previesState)
-
-// objCounter.count = 4
-// console.log(previesState)
-
-
-//immutable
-const previesState2 = {...objCounter} // shallow copy
-console.log(previesState2)
-
-objCounter.count = 10
-console.log(previesState2)
+var newV = updateCounter()
+console.log(newV)
+newV = updateCounter()
+console.log(newV)
+var newV = updateCounter()
+console.log(newV)
+newV = updateCounter()
+console.log(newV)
