17516c02abc0ec97f704efaeb012358afda0658f
M-Asimm <kpkprince55persia@gmail.com>
2024-03-05 11:27:07 +0500

    redux reducer and redux action type is covered

--- corejs-redux/js/index.js
+++ corejs-redux/js/index.js
@@ -59,20 +59,84 @@
 // updateCounter();
 // console.log(counter);
 
-let counter = {
+// let counter = {
+//   count: 0,
+// };
+// let previesState  = counter
+
+// // console.log(previesState);
+
+// immutable
+// function updateCounter() {
+//   return counter = { count: counter.count + 1 };
+// }
+// var newV = updateCounter()
+// console.log(newV)
+// newV = updateCounter()
+// console.log(newV)
+// var newV = updateCounter()
+// console.log(newV)
+// newV = updateCounter()
+// console.log(newV)
+
+// let student = {
+//   name: "ahmad",
+//   age: 18,
+//   counter: 0,
+// };
+
+// function updateState() {
+//   return (student = { ...student, counter: student.counter + 1 });
+// }
+
+// console.log(updateState());
+// console.log(updateState());
+// console.log(updateState());
+// console.log(updateState());
+
+/**
+ * @redux_reducer function
+ */
+
+// let student = {
+//   name: "kamran",
+//   count: 0,
+// };
+
+// function reducer(state) {
+//   return { ...state, count: state.count + 1 };
+// }
+
+// const state1Student = reducer(student)
+// console.log(state1Student)
+// const state2Student = reducer(state1Student)
+// console.log(state2Student)
+// const state3Student = reducer(state2Student)
+// console.log(state3Student)
+
+/**
+ * @redux_action
+ * @param { action:{ type: something, payload: something } }
+ */
+
+let student = {
+  name: "kamran",
   count: 0,
 };
-let previesState  = counter
 
-// console.log(previesState);
-function updateCounter() {
-  return counter = { count: counter.count + 1 };
+function reducer(state, action) {
+  if (action.type === "INCREMENT") {
+    return { ...state, count: state.count + 1 };
+  } else if (action.type === "DECREMENT") {
+    return { ...state, count: state.count - 1 };
+  } else {
+    return state;
+  }
 }
-var newV = updateCounter()
-console.log(newV)
-newV = updateCounter()
-console.log(newV)
-var newV = updateCounter()
-console.log(newV)
-newV = updateCounter()
-console.log(newV)
+
+const state1Student = reducer(student, { type: "INCREMENT" });
+console.log(state1Student);
+const state2Student = reducer(student, { type: "DECREMENT" });
+console.log(state2Student);
+const state3Student = reducer(student, { type: "kana banoo" });
+console.log(state3Student);
