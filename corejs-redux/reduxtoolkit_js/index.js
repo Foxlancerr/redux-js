870f95d2f4b7f3d00a2259839f9f083d4cbbbbf4
M-Asimm <kpkprince55persia@gmail.com>
2024-03-05 14:49:00 +0500

    redux pure js is completed

--- corejs-redux/reduxtoolkit_js/index.js
+++ corejs-redux/reduxtoolkit_js/index.js
@@ -5,17 +5,21 @@ let initailCounter = {
   count: 0,
 };
 
-// 
+//
 // this will create a centralized store
 // the createStore can takes reducer function as callback
-const store = createStore(reducer);
+const store = createStore(reducer); // this is old methods
 
-function reducer(state = initailCounter, action) {
+function reducer(state=initailCounter, action) {
   switch (action.type) {
     case "Increment":
       return { ...state, count: state?.count + 1 };
     case "Decrement":
       return { ...state, count: state?.count - 1 };
+    case "DecrementBY10":
+      return { ...state, count: state?.count - action.payload };
+    case "IncrementBY10":
+      return { ...state, count: state?.count + action.payload };
     default:
       return state;
   }
@@ -26,15 +30,9 @@ store.subscribe(() => {
 });
 
 store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
 store.dispatch({ type: "Decrement" });
-store.dispatch({ type: "Decrement" });
-store.dispatch({ type: "Decrement" });
-store.dispatch({ type: "Decrement" });
-store.dispatch({ type: "Decrement" });
-store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
-store.dispatch({ type: "Increment" });
+store.dispatch({ type: "DecrementBY10" ,payload: 10});
+store.dispatch({ type: "IncrementBY10" ,payload: 10});
+store.dispatch({ type: "IncrementBY10" ,payload: 10});
+store.dispatch({ type: "IncrementBY10" ,payload: 10});
+
