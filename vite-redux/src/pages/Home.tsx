5b48468183a324292ba824d25c8fd9b8e59cc5e8
M-Asimm <kpkprince55persia@gmail.com>
2024-03-10 21:44:32 +0500

    product cart style is done

--- vite-redux/src/pages/Home.tsx
+++ vite-redux/src/pages/Home.tsx
@@ -3,27 +3,29 @@ import productData from "../assets/productData";
 
 function Home() {
   return (
-    <div className="px-20 pt-10 gap-3 flex justify-between w-full flex-wrap">
-      {productData.map((productItem) => (
-        <div
-          className="bg-slate-200/20 w-[350px] p-5 rounded"
-          key={productItem.id}
-        >
-          <img
-            className="object-cover w-[250px] h-[250px]"
-            src={productItem.image}
-            alt={productItem.image + productItem.id}
-          />
-
-          <div>
-            <h1 className="text-2xl font-medium pt-2">{productItem.title}</h1>
-            <p>{productItem.description.substring(0, 10)}</p>
-            <button className="w-[50%] bg-black font-bold text-white py-2">
-              add to cart
-            </button>
-            <button className="w-[50%] bg-blue-700 font-bold text-white py-2">
-              buy now
-            </button>
+    <div className="px-20 pt-10 flex flex-wrap justify-between">
+      {productData.map((product) => (
+        <div className="bg-slate-200 w-96 p-5 mb-10">
+          <div className="bg-red-400 w-full h-[250px] overflow-hidden">
+            <img src={product.image} alt="product Image" className="object-fill w-auto" />
+          </div>
+          <div className="mt-2">
+            <h1 className="text-2xl font-medium">{product.title}</h1>
+            <p className="text-black/70">
+              {product.description}
+            </p>
+            <div className="flex item-center gap-2">
+              <p>Rating: {product.rating.rate}</p>
+              <p>Price: ${product.price}</p>
+            </div>
+            <div className="flex gap-5 mt-2 w-full">
+              <button className="bg-black text-white py-2 px-6 w-[50%]">
+                Add to Cart
+              </button>
+              <button className="bg-blue-700 text-white py-2 px-6 w-[50%]">
+                Buy Now
+              </button>
+            </div>
           </div>
         </div>
       ))}
